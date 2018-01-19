04808b6e63a804d333a6511caef9092d98572ac3
Hans Christian Nenseth <nenseth@gmail.com>
2018-01-19 16:40:25 +0100

    more refactoring

--- england_flight.py
+++ england_flight.py
@@ -13,15 +13,14 @@ import time
 
 def check_flights():
     url = "https://www.google.com/flights/explore/#explore;f=OSL,TRF,RYG;t=r-England,+United+Kingdom-0x47d0a98a6c1ed5df%253A0xf4e19525332d8ea8;li=3;lx=5;d=2018-01-24"
-    driver = webdriver.PhantomJS()
     dcap = dict(DesiredCapabilities.PHANTOMJS)
-    dcap["phantomjs.page.settings.userAgent"] = ("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36")
+    dcap["phantomjs.page.settings.userAgent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36"
     driver = webdriver.PhantomJS(desired_capabilities=dcap, service_args=['--ignore-ssl-error=true'])
     driver.implicitly_wait(20)
     driver.get(url)
 
-    wait =WebDriverWait(driver, 20)
-    wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR,"span.CTPFVNB-v-c")))
+    wait = WebDriverWait(driver, 20)
+    wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "span.CTPFVNB-v-c")))
 
     s = BeautifulSoup(driver.page_source, "lxml")
 
