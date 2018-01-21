fa237ce6e595d43b355bbb3d30d91ce907fe4737
Hans Christian Nenseth <nenseth@gmail.com>
2018-01-22 00:08:40 +0100

    removed unneeded imports

--- england_flight.py
+++ england_flight.py
@@ -8,7 +8,6 @@ import schedule
 import time
 
 from selenium import webdriver
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.common.by import By
 
 from selenium.webdriver.support.ui import WebDriverWait
